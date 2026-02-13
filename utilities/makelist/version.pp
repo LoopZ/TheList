b2abd72b7cfe7e257faadeb07aa9d8ac42dbd947
Shidel <jerome@shidel.net>
2026-02-13 18:44:24 -0500

    added parse_results to aid in manual cleanup process

--- utilities/makelist/version.pp
+++ utilities/makelist/version.pp
@@ -26,9 +26,9 @@ const
 
   { Source version and most recent project commit }
   SOURCE_VERSION = '1.0.0';
-  SOURCE_REVISION = '29';
+  SOURCE_REVISION = '20';
   SOURCE_URL = 'http://github.com/LoopZ/TheList';
-  SOURCE_COMMIT = '702b4495f0402b0a6b995b9b48be80241adeaf3f';
+  SOURCE_COMMIT = 'a7e9e852cc3159a11af7156770153b33f9a7ce29';
 
   { Version Build Atributes } 
   BUILD_DEBUG: Boolean = False;
@@ -36,7 +36,7 @@ const
   BUILD_PATCHED: Boolean = False;
   BUILD_PRIVATE: Boolean = False;
   BUILD_SPECIAL: Boolean = False;
-  BUILD_DATE: String = '2026-02-13 17:50:00';
+  BUILD_DATE: String = '2026-02-13 18:38:52';
 
   { General Application Information }
   APP_VERSION: String = '1.0.0';
