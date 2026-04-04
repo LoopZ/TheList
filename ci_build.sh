3e179a6a2b9d66ab4bf066bf1a0d0dc1d6a2084c
Andrew Bird <ajb@spheresystems.co.uk>
2026-04-04 09:27:16 +0100

    CI: Move makelist build to separate step

--- ci_build.sh
+++ ci_build.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# Let's build makelist ourselves as we want to be sure that it's the latest
-(cd utilities
-  [ -f bin/makelist ] && mv bin/makelist bin/makelist.repo
-  fpc -obin/makelist makelist/makelist.lpr
-)
-
-echo
-
-# Now let's build the TheList
+# Let's build the TheList
 ./utilities/bin/makelist -s source -o TheList
 
