7c5be33440d8e9d7bd2933a90a9693ad4cce8953
Shidel <jerome@shidel.net>
2026-04-09 03:54:56 -0400

    create bin dir when it does not exist

--- ci_build_tools.sh
+++ ci_build_tools.sh
@@ -4,6 +4,7 @@ set -e
 
 # Let's build makelist ourselves as we want to be sure that it's the latest
 (cd utilities
+  [ ! -d bin ] && mkdir bin
   [ -f bin/makelist ] && mv bin/makelist bin/makelist.repo
   fpc -obin/makelist makelist/makelist.lpr
 )
